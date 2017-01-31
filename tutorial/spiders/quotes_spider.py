3af21cf3f475430783e46574e8597d90066b770e
Hengji <hengji.liu@outlook.com>
2017-01-31 19:03:22 +1100

    commented

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -2,9 +2,14 @@ import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
-    name = "quotes"
+    name = "quotes"  # identifies the spider, unique in a project
 
     def start_requests(self):
+        """
+
+        :return: an iterable of Requests, either a list or a generator
+        """
+
         urls = [
             'http://quotes.toscrape.com/page/1/',
             'http://quotes.toscrape.com/page/2/',
@@ -13,6 +18,15 @@ class QuotesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        """
+        Parses the response:
+        1. extract data as dicts
+        2. find new URLs to follow
+        3. create new requests
+
+        :param response:
+        :return:
+        """
         page = response.url.split("/")[-2]
         filename = 'quotes-%s.html' % page
         with open(filename, 'wb') as f:
